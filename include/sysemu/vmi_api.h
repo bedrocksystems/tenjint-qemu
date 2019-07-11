defb27bea624640e4de3d6a7b4e868e06f639968
Jonas Pfoh <jonas@bedrocksystems.com>
2019-07-11 17:50:30 +0200

    cython fixes

--- include/sysemu/vmi_api.h
+++ include/sysemu/vmi_api.h
@@ -1,13 +1,11 @@
 #ifndef VMI_API_H
 #define VMI_API_H
-#include "qemu/osdep.h"
-
 #include <linux/kvm_vmi.h>
 
-int vmi_api_init(void);
-void vmi_api_uninit(void);
+extern int vmi_api_init(void);
+extern void vmi_api_uninit(void);
 
-union kvm_vmi_event* vmi_api_get_event(void);
-void vmi_api_wait_event(void);
+extern union kvm_vmi_event* vmi_api_get_event(void);
+extern void vmi_api_wait_event(void);
 
 #endif
