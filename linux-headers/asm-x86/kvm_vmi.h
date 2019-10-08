7187b5671e140ddd500d1036d10d839efb20caa0
Jonas Pfoh <jonas@bedrocksystems.com>
2019-10-08 15:02:40 +0200

    handle debug feature request
    This commit adds special handling for the debug feature request as
    this doesnt go to the kernel as a vmi feature request.

--- linux-headers/asm-x86/kvm_vmi.h
+++ linux-headers/asm-x86/kvm_vmi.h
@@ -32,7 +32,8 @@
 #define KVM_VMI_FEATURE_LBR               1
 #define KVM_VMI_FEATURE_MTF               2
 #define KVM_VMI_FEATURE_SLP               3
-#define KVM_VMI_FEATURE_MAX               4
+#define KVM_VMI_FEATURE_DEBUG             4
+#define KVM_VMI_FEATURE_MAX               5
 
 #define KVM_VMI_EVENT_TASK_SWITCH         0
 #define KVM_VMI_EVENT_DEBUG               1
@@ -40,17 +41,17 @@
 #define KVM_VMI_EVENT_SLP                 3
 
 struct kvm_vmi_feature_task_switch {
-    __u32 feature;
-    __u8 enable;
-    __u64 dtb;
-    __u8 incoming;
-    __u8 outgoing;
+	__u32 feature;
+	__u8 enable;
+	__u64 dtb;
+	__u8 incoming;
+	__u8 outgoing;
 };
 
 struct kvm_vmi_feature_lbr {
-    __u32 feature;
-    __u8 enable;
-    __u64 lbr_select;
+	__u32 feature;
+	__u8 enable;
+	__u64 lbr_select;
 };
 
 struct kvm_vmi_feature_mtf {
@@ -59,65 +60,73 @@ struct kvm_vmi_feature_mtf {
 };
 
 struct kvm_vmi_feature_slp {
-    __u32 feature;
-    __u8 enable;
-    __u8 global_req;
-    __u64 gfn;
-    __u64 num_pages;
-    __u64 violation;
+	__u32 feature;
+	__u8 enable;
+	__u8 global_req;
+	__u64 gfn;
+	__u64 num_pages;
+	__u64 violation;
+};
+
+struct kvm_vmi_feature_debug {
+	__u32 feature;
+	__u8 enable;
+	__u8 single_step;
+	__u8 watchpoint;
+	__u64 addr;
 };
 
 union kvm_vmi_feature {
-    __u32 feature;
-    struct kvm_vmi_feature_task_switch ts;
-    struct kvm_vmi_feature_lbr lbr;
+	__u32 feature;
+	struct kvm_vmi_feature_task_switch ts;
+	struct kvm_vmi_feature_lbr lbr;
 	struct kvm_vmi_feature_mtf mtf;
-    struct kvm_vmi_feature_slp slp;
+	struct kvm_vmi_feature_slp slp;
 };
 
 struct kvm_vmi_event_task_switch {
-    __u32 type;
-    __u32 cpu_num;
-    __u64 old_cr3;
-    __u64 new_cr3;
+	__u32 type;
+	__u32 cpu_num;
+	__u64 old_cr3;
+	__u64 new_cr3;
 };
 
 struct kvm_vmi_event_debug {
-    __u32 type;
-    __u32 cpu_num;
-    __u8 single_step;
-    __u8 watchpoint;
-    __u64 watchpoint_gva;
-    __s32 watchpoint_flags;
+	__u32 type;
+	__u32 cpu_num;
+	__u8 single_step;
+	__u8 watchpoint;
+	__u64 watchpoint_gva;
+	__s32 watchpoint_flags;
 };
 
 struct kvm_vmi_event_mtf {
-    __u32 type;
-    __u32 cpu_num;
+	__u32 type;
+	__u32 cpu_num;
 };
 
 struct kvm_vmi_event_slp {
-    __u32 type;
-    __u32 cpu_num;
-    __u64 violation;
-    __u64 gva;
-    __u64 gpa;
-    __u8 rwx;
+	__u32 type;
+	__u32 cpu_num;
+	__u64 violation;
+	__u64 gva;
+	__u64 gpa;
+	__u8 rwx;
 };
 
 union kvm_vmi_event {
-    __u32 type;
-    struct kvm_vmi_event_task_switch ts;
-    struct kvm_vmi_event_debug debug;
-    struct kvm_vmi_event_mtf mtf;
-    struct kvm_vmi_event_slp slp;
+	__u32 type;
+	struct kvm_vmi_event_task_switch ts;
+	struct kvm_vmi_event_debug debug;
+	struct kvm_vmi_event_mtf mtf;
+	struct kvm_vmi_event_slp slp;
 };
 
 struct kvm_vmi_lbr_info {
-    __u32 entries;
-    __u8 tos;
-    __u64 lbr_from[MAX_LBR_ENTRIES];
-    __u64 lbr_to[MAX_LBR_ENTRIES];
+	__u32 entries;
+	__u8 tos;
+	__u64 lbr_from[MAX_LBR_ENTRIES];
+	__u64 lbr_to[MAX_LBR_ENTRIES];
 };
 
 #define KVM_VMI_GET_LBR           _IOW(KVMIO, 0xf8, struct kvm_vmi_lbr_info)
