aec626743d237c7912ca4eb894f1479b12853458
Sebastian Vogl <sebastian@bedrocksystems.com>
2019-10-09 12:23:54 +0200

    Provides initial support aarch64 breakpoints

--- linux-headers/asm-arm64/kvm_vmi.h
+++ linux-headers/asm-arm64/kvm_vmi.h
@@ -27,57 +27,80 @@
 
 #define KVM_VMI_FEATURE_TRAP_TASK_SWITCH  0
 #define KVM_VMI_FEATURE_SLP               1
-#define KVM_VMI_FEATURE_MAX               2
+#define KVM_VMI_FEATURE_DEBUG             2
+#define KVM_VMI_FEATURE_MAX               3
 
 #define KVM_VMI_EVENT_TASK_SWITCH         0
 #define KVM_VMI_EVENT_SLP                 1
+#define KVM_VMI_EVENT_DEBUG               2
 
 #define KVM_VMI_TTBR0                     0
 #define KVM_VMI_TTBR1                     1
 #define KVM_VMI_TCR                       2
 
 struct kvm_vmi_feature_task_switch {
-    __u32 feature;
-    __u8 enable;
-    __u8 reg;
+	__u32 feature;
+	__u8 enable;
+	__u8 reg;
 };
 
 struct kvm_vmi_feature_slp {
-    __u32 feature;
-    __u8 enable;
-    __u8 global_req;
-    __u64 gfn;
-    __u64 num_pages;
-    __u64 violation;
+	__u32 feature;
+	__u8 enable;
+	__u8 global_req;
+	__u64 gfn;
+	__u64 num_pages;
+	__u64 violation;
+};
+
+struct kvm_vmi_feature_debug {
+	__u32 feature;
+	__u8 enable;
+	__u8 single_step;
+	__u8 watchpoint;
+	__u64 addr;
 };
 
 union kvm_vmi_feature {
-    __u32 feature;
-    struct kvm_vmi_feature_task_switch ts;
-    struct kvm_vmi_feature_slp slp;
+	__u32 feature;
+	struct kvm_vmi_feature_task_switch ts;
+	struct kvm_vmi_feature_slp slp;
+	struct kvm_vmi_feature_debug debug;
 };
 
 struct kvm_vmi_event_task_switch {
-    __u32 type;
-    __u32 cpu_num;
-    __u8 reg;
-    __u64 old_val;
-    __u64 new_val;
+	__u32 type;
+	__u32 cpu_num;
+	__u8 reg;
+	__u64 old_val;
+	__u64 new_val;
 };
 
 struct kvm_vmi_event_slp {
-    __u32 type;
-    __u32 cpu_num;
-    __u64 violation;
-    __u64 gva;
-    __u64 gpa;
-    __u8 rwx;
+	__u32 type;
+	__u32 cpu_num;
+	__u64 violation;
+	__u64 gva;
+	__u64 gpa;
+	__u8 rwx;
+};
+
+struct kvm_vmi_event_debug {
+	__u32 type;
+	__u32 cpu_num;
+	__u8 single_step;
+	__u8 watchpoint;
+	__u64 watchpoint_gva;
+	__s32 watchpoint_flags;
+	__u64 breakpoint_gva;
+	__u64 breakpoint_gpa;
 };
 
 union kvm_vmi_event {
-    __u32 type;
-    struct kvm_vmi_event_task_switch ts;
-    struct kvm_vmi_event_slp slp;
+	__u32 type;
+	struct kvm_vmi_event_task_switch ts;
+	struct kvm_vmi_event_slp slp;
+	struct kvm_vmi_event_debug debug;
 };
 
 #endif /* _ASM_ARM64_KVM_VMI_H */
