ef5ccfbcecd9bdaa2a55717945864525b6f8b857
Jonas Pfoh <jonas@bedrocksystems.com>
2019-10-11 16:54:58 +0200

    add arm qemu translation

--- target/i386/vmi.c
+++ target/i386/vmi.c
@@ -11,10 +11,7 @@ uint64_t vmi_api_arch_vtop(uint64_t addr, uint64_t dtb) {
     uint64_t rv;
 
     memcpy(&trans_cpu, cpu, sizeof(X86CPU));
-
-    trans_cpu.env.cr[0] = cpu->env.cr[0];
     trans_cpu.env.cr[3] = dtb;
-    trans_cpu.env.cr[4] = cpu->env.cr[4];
 
     rv = cpu_get_phys_page_debug(trans_cs, addr & TARGET_PAGE_MASK);
     rv += addr & ~TARGET_PAGE_MASK;
