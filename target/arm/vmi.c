bd90ba8880c7008c95503f6884720e520620338b
Jonas Pfoh <jonas@bedrocksystems.com>
2019-10-14 15:47:12 +0000

    fix arm translation error

--- target/arm/vmi.c
+++ target/arm/vmi.c
@@ -3,6 +3,7 @@
 
 #include "sysemu/vmi_api.h"
 
+// this returns (uint64_t)-1 on failure
 uint64_t vmi_api_arch_vtop(uint64_t addr, uint64_t dtb) {
     ARMCPU trans_cpu;
     CPUState *trans_cs = CPU(&trans_cpu);
@@ -12,19 +13,19 @@ uint64_t vmi_api_arch_vtop(uint64_t addr, uint64_t dtb) {
 
     memcpy(&trans_cpu, cpu, sizeof(ARMCPU));
 
-    trans_cpu.env.cp15.ttbr0_el[1] = addr;
-    // trans_cpu.env.cp15.ttbr1_el[1] = 
-    // trans_cpu.env.cp15.tcr_el[1].raw_tcr = 
-    // trans_cpu.env.cp15.tcr_el[1].mask = 
-    // trans_cpu.env.cp15.tcr_el[1].base_mask = 
+    // printf("--------------------------------------------------------\n");
+    // printf("trans_cpu.env.cp15.ttbr0_el[1] = 0x%lx\n", trans_cpu.env.cp15.ttbr0_el[1]);
+    // printf("trans_cpu.env.cp15.ttbr1_el[1] = 0x%lx\n", trans_cpu.env.cp15.ttbr1_el[1]);
+    // printf("trans_cpu.env.cp15.tcr_el[1].raw_tcr = 0x%lx\n", trans_cpu.env.cp15.tcr_el[1].raw_tcr);
+    // printf("trans_cpu.env.cp15.tcr_el[1].mask = 0x%x\n", trans_cpu.env.cp15.tcr_el[1].mask);
+    // printf("trans_cpu.env.cp15.tcr_el[1].base_mask = 0x%x\n", trans_cpu.env.cp15.tcr_el[1].base_mask);
+    // printf("--------------------------------------------------------\n");
 
-    printf("--------------------------------------------------------\n");
-    printf("trans_cpu.env.cp15.ttbr0_el[1] = 0x%lx\n", trans_cpu.env.cp15.ttbr0_el[1]);
-    printf("trans_cpu.env.cp15.ttbr1_el[1] = 0x%lx\n", trans_cpu.env.cp15.ttbr1_el[1]);
-    printf("trans_cpu.env.cp15.tcr_el[1].raw_tcr = 0x%lx\n", trans_cpu.env.cp15.tcr_el[1].raw_tcr);
-    printf("trans_cpu.env.cp15.tcr_el[1].mask = 0x%x\n", trans_cpu.env.cp15.tcr_el[1].mask);
-    printf("trans_cpu.env.cp15.tcr_el[1].base_mask = 0x%x\n", trans_cpu.env.cp15.tcr_el[1].base_mask);
-    printf("--------------------------------------------------------\n");
+    trans_cpu.env.cp15.ttbr0_el[1] = dtb;
+    trans_cpu.env.cp15.ttbr1_el[1] = 0x41119000;
+    trans_cpu.env.cp15.tcr_el[1].raw_tcr = 0x32b5503510;
+    trans_cpu.env.cp15.tcr_el[1].mask = 0x0;
+    trans_cpu.env.cp15.tcr_el[1].base_mask = 0xffffc000;
 
     rv = cpu_get_phys_page_debug(trans_cs, addr & TARGET_PAGE_MASK);
     rv += addr & ~TARGET_PAGE_MASK;
