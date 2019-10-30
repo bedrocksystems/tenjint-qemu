267a4796f6c48eb36aade52adc991d60b6f720de
Jonas Pfoh <jonas@bedrocksystems.com>
2019-10-30 14:58:39 +0100

    fix arm vtop

--- target/arm/vmi.c
+++ target/arm/vmi.c
@@ -9,7 +9,7 @@ uint64_t vmi_api_arch_vtop(uint64_t addr, uint64_t dtb) {
     CPUState *trans_cs = CPU(&trans_cpu);
     CPUState *cs = qemu_get_cpu(0);
     ARMCPU *cpu = ARM_CPU(cs);
-    uint64_t rv;
+    uint64_t rv, tcr;
 
     memcpy(&trans_cpu, cpu, sizeof(ARMCPU));
 
@@ -21,11 +21,13 @@ uint64_t vmi_api_arch_vtop(uint64_t addr, uint64_t dtb) {
     // printf("trans_cpu.env.cp15.tcr_el[1].base_mask = 0x%x\n", trans_cpu.env.cp15.tcr_el[1].base_mask);
     // printf("--------------------------------------------------------\n");
 
+    tcr = trans_cpu.env.cp15.tcr_el[1].raw_tcr;
+
     trans_cpu.env.cp15.ttbr0_el[1] = dtb;
-    trans_cpu.env.cp15.ttbr1_el[1] = 0x41119000;
-    trans_cpu.env.cp15.tcr_el[1].raw_tcr = 0x32b5503510;
-    trans_cpu.env.cp15.tcr_el[1].mask = 0x0;
-    trans_cpu.env.cp15.tcr_el[1].base_mask = 0xffffc000;
+    trans_cpu.env.cp15.ttbr1_el[1] = 0;
+    trans_cpu.env.cp15.tcr_el[1].raw_tcr = tcr & 0xffffffffffffffc0;
+    trans_cpu.env.cp15.tcr_el[1].mask = 0;
+    trans_cpu.env.cp15.tcr_el[1].base_mask = 0;
 
     rv = cpu_get_phys_page_debug(trans_cs, addr & TARGET_PAGE_MASK);
     rv += addr & ~TARGET_PAGE_MASK;
